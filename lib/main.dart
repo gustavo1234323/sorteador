6761542003f759996a42870547ea31c4bd62564b
joao.veloso.gois@escola.pr.gov.br <joao.veloso.gois@escola.pr.gov.br>
2025-06-16 08:36:47 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:helloword/telas/tela_pricipal.dart';
+
+void main()  {
+runApp(
+  MaterialApp(
+    title: "guzin 777",
+    home: HomePage(),
+  )
+);
+}
